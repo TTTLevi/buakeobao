814b7d33c51221e2f38a70d8a82f1ae6f31f8ab2
Lev <thientong712001@gmail.com>
2024-06-04 18:28:50 +0700

    keydown

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -52,6 +52,18 @@ document.querySelector('.scissors').addEventListener('click',() => {
   playGame('scissors');
 })
 
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'r'){
+    playGame('rock');
+  }
+  else if (event.key === 'p'){
+    playGame('paper');
+  }
+  else if (event.key === 's'){
+    playGame('scissors');
+  }
+})
+
 function playGame(playerMove){
   let result = '';
   const computerMove = pickComputerMove();
